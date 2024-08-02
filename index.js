abbaa72eb33bf2f34bb1d1ad1e62b5c41e2e4366
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-08-02 22:27:34 +0500

    4--oy 9-dars

---
+++ index.js
@@ -0,0 +1,114 @@
+
+// 1-masla
+// let arr = [1,2,3,4,5];
+// function test(arr) {
+//     let res = [];
+//     arr.forEach(function(a , i , b) {
+//         res.push(a*2); 
+//     });
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 2-masla
+// let arr = ['salom' , 'hello' , 'hayr'];
+// function test(arr) {
+//     let res = [];
+//     arr.forEach(function(a , i , b) {
+//         res.push(a.toUpperCase()); 
+//     });
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 3-masla
+// let arr = [1,2,3,4,5];
+// function test(arr) {
+//     let res = [];
+//     arr.forEach(function(a , i , b) {
+//         res.push(a+5); 
+//     });
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 1-masla
+// let arr = [1,2,3,4,5];
+// function test(arr) {
+//    let res = arr.map(function (a) {
+//     return a * 2
+//    }) 
+//    return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 2-masla
+// let arr = ['salom' , 'hello' , 'hayr'];
+// function test(arr) {
+//     let res = arr.map(function (a) {
+//         return a.toUpperCase()
+//     })
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 3-masala
+// let arr = [1,2,3,4,5];
+// function test(arr) {
+//    let res = arr.map(function (a) {
+//     return a + 5
+//    }) 
+//    return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 1-masla
+// let arr = [1,2,3,4,5];
+// function test(arr) {
+// let res = arr.filter(function(a) {
+//   if (a%2==0) {
+//     return a;
+//   }  
+// })
+// return res;
+// }
+// 2-masla
+// let arr = ['salomat' , 'hello' , 'hayrli kech'];
+// function test(arr) {
+//     let res = arr.filter(function (a) {
+//         if (a.length > 5) {
+//             return a;
+//         }
+//     })
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 3-masla
+// let arr = [14,24,17,19];
+// function test(arr) {
+//     let res = arr.filter(function (a) {
+//         if (a > 18) {
+//             return a
+//         }
+//     })
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+// 1-masala
+// let arr = [14,24,17,19];
+// function test(arr) {
+//     return arr.find(arr => arr > 10);
+//   }
+//   let res = test(arr);
+// console.log(res);
+// 1-masala
+// let arr = [-14,-24,17,-19];
+// function test(arr) {
+//     return arg.some(function(a) {
+//         return a > 0
+//     });
+//   }
+//   let res = test(arr);
+// console.log(res);
